87983cbf9e1e948a1d122ca318765edb08a37aa0
kieudaithien1 <thienkieu@yggsea.io>
2022-08-15 15:41:54 +0700

    update code

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const json = {
-    '0': '0xC7446945830E99389AD2fd29707034f998bF65d8',
-    '1': '0x4b023d85601B6101CC6EE073E85A689D521e0B78',
-    '2': '400000000000000000000',
-    from: '0xC7446945830E99389AD2fd29707034f998bF65d8',
-    to: '0x4b023d85601B6101CC6EE073E85A689D521e0B78',
-    value: '400000000000000000000'
-  }
-console.log(json[0])
+const { LAMPORTS_PER_SOL } = require("@solana/web3.js");
+const { transferSplToken } = require("./lib/solana/token")
+const bs58 = require("bs58");
+
+(async()=>{
+    await transferSplToken(
+      "4a5gQjCLrUnS5MTGH6427RXWanTWGytRoT7WYHhj7d3Z9XVFmTreW81pXC3v1mzwPZkFvqJ4MXsasLwThQRbYXHD",
+      "HbUfzDvVH3r5Y28i7U3XRCFfsud1ZwizaGKP5cAVgn9",
+      "6ULxRpk6t4EYTfZnwKVste2hzQ8BQzF9KPdcwiu3ha2a",
+      1
+    )
+})()
