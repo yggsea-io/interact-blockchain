2ea4fd1bf33eae475b605873028104e4c7e4a436
kieudaithien1 <thienkieu@yggsea.io>
2022-10-04 11:06:22 +0700

    Update api and handle register game dracoo

---
+++ src/task-ether/dracoo-master/register/register.js
@@ -0,0 +1,24 @@
+require("../../../common/ether/network").useBSC()
+const path = require('path')
+const { readData } = require("../../../common/excel/csv")
+const { runMutiThreadForLoop } = require("../../../common/utils")
+const Api = require('./api')
+const GmailAPI = require("../../../common/gmail/GmailApi");
+require('dotenv').config({ path: path.join(__dirname, '../../../../.env') });
+const gmailApi = new GmailAPI(process.env.REFRESH_TOKEN_MAIL, process.env.ACCESS_TOKEN_MAIL);
+
+
+main().catch(err => console.log(err))
+async function main(){
+    const accounts = await readData(path.join(__dirname, './account.txt'))
+    const deleteDracoo = await gmailApi.deleteGmailFrom("workspace-noreply@google.com");
+    await runMutiThreadForLoop(accounts.length, 0, register, accounts)
+}
+
+async function register(start, end, data){
+    for(let i = start; i< end; i++){
+        let id = data[i].id.match(/: [^\s]+/g, "")[0].replace(": ", "")
+        let api = new Api(data[i].address, gmailApi)
+        const regiter = await api.registerGame(`yggsea${id}`, `dracoobinding+${id}@yggsea.io`)
+    }
+}
