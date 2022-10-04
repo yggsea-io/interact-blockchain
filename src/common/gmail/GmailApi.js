d16d7ea4ed89ff151a3659961ee63ddd569b82f0
kieudaithien1 <thienkieu@yggsea.io>
2022-10-04 11:04:48 +0700

    init funtion delete for gmail api

--- src/common/gmail/GmailApi.js
+++ src/common/gmail/GmailApi.js
@@ -3,24 +3,28 @@ var qs = require("qs");
 
 class GmailAPI {
   accessToken = "";
-  refresh_token = undefined
+  refresh_token = undefined;
   constructor(refresh_token, accessToken) {
     this.refresh_token = refresh_token;
-    this.accessToken = (accessToken != undefined) ? accessToken : this.getAcceToken();
+    this.accessToken =
+      accessToken != undefined ? accessToken : this.getAcceToken();
   }
 
   setAccessToken = async () => {
     var req = {
-      'token_uri' : "https://oauth2.googleapis.com/token",
-      'refresh_token': this.refresh_token
+      token_uri: "https://oauth2.googleapis.com/token",
+      refresh_token: this.refresh_token,
     };
 
-    const { data } = await axios.post("https://developers.google.com/oauthplayground/refreshAccessToken", req)
-    if(data.error != undefined){
-        console.log(`Err: ${error}`)
-        return this.accessToken
+    const { data } = await axios.post(
+      "https://developers.google.com/oauthplayground/refreshAccessToken",
+      req
+    );
+    if (data.error != undefined) {
+      console.log(`Err: ${error}`);
+      return this.accessToken;
     }
-    return data.access_token; 
+    return data.access_token;
   };
 
   searchGmail = async (searchItem) => {
@@ -38,10 +42,37 @@ class GmailAPI {
       .then(async function (response) {
         idMess = await response.data["messages"];
       })
-      .catch(function (error) {
-      });
+      .catch(function (error) {});
 
     return idMess;
+  };
+  deleteGmailFrom = async (searchText) => {
+    try {
+      const messIds = await this.searchGmail(searchText);
+      if(messIds == undefined){
+        console.log('Not mail to delete')
+        return true
+      }
+
+      var config1 = async (id) => {
+        return {
+          method: "delete",
+          url: `https://gmail.googleapis.com/gmail/v1/users/me/messages/${id}`,
+          headers: {
+            Authorization: `Bearer ${await this.accessToken} `,
+          },
+        };
+      };
+      for (let item of messIds) {
+        const req = await config1(item.id);
+        await axios(req).catch((err) => {
+          return false;
+        });
+      }
+      return true;
+    } catch (error) {
+      return false
+    }
   };
 
   readGmailContent = async (messageId) => {
@@ -67,14 +98,18 @@ class GmailAPI {
   };
 
   readInboxContent = async (searchText) => {
-    const messIds = await this.searchGmail(searchText);
-    let messages = []
-    
-    for(let item of messIds){
-      const mess = await this.readGmailContent(item.id);
-      messages.push(mess)
+    try {
+      const messIds = await this.searchGmail(searchText);
+      let messages = [];
+
+      for (let item of messIds) {
+        const mess = await this.readGmailContent(item.id);
+        messages.push(mess);
+      }
+      return messages;
+    } catch (error) {
+      return error;
     }
-    return messages;
   };
 }
 
