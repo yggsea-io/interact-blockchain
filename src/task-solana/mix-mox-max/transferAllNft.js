ffd4c94c6b6b3571a634cb1ef4894e546ffb117d
kieudaithien1 <thienkieu@yggsea.io>
2022-10-10 15:39:17 +0700

    handle transfer all nft of mix-mox-max on solana

---
+++ src/task-solana/mix-mox-max/transferAllNft.js
@@ -0,0 +1,55 @@
+const { Connection, clusterApiUrl } = require("@solana/web3.js");
+require('dotenv').config({ path: path.join(__dirname, '../../.env') });
+const { default: axios } = require("axios");
+const { transferNFT } = require("../../common/solana/nft");
+const {runMutiThreadForLoop } = require("../../common/utils");
+const connection = new Connection(clusterApiUrl("mainnet-beta"));
+const ownerKeyPair = process.env.YGGSEA
+
+main().catch(err => console.log(err))
+let nfts = []
+async function main(){
+    let { data } = await axios.get(`https://api.solscan.io/account/v2/tokenaccounts/total?address=${ownerKeyPair.publicKey}&cluster=`)
+    const totalNft = data.data
+    //get all nft
+    await runMutiThreadForLoop(totalNft, 0, getNfts)
+    // transfer all nft
+    await runMutiThreadForLoop(nfts.length, 0, transferAllNft)
+    
+}
+
+
+async function getNfts(start, end) {
+    var dataHandle = [];
+    var offset = start;
+    let limit = 10
+    do {
+      const { data } = await axios.get(
+        "https://api.solscan.io/account/v2/tokenaccounts",
+        {
+          params: {
+            address: ownerKeyPair.publicKey,
+            offset: offset,
+            limit: limit,
+            cluster: ''
+          },
+        }
+      );
+      dataHandle = data.data;
+      for (let item of dataHandle) {
+        if(item.tokenSymbol != 'MXM')
+            continue
+        nfts.push(item)
+      }
+      offset += 10;
+      limit = (offset + 10 > end ) ? (end - offset) : 10
+    } while (dataHandle != "[]" && offset <= end);
+  }
+
+async function transferAllNft(start, end){
+    for(let i = start; i < end; i++){
+        if(nfts[i].tokenSymbol != 'MXM')
+            continue
+        await transferNFT(connection, fromWallet, nfts[i].tokenAddress, 'ACPpwoSRF4B4Ftsjj9TAGxBmJwkHZNb13jnYdEiRshmA')
+    }
+}
