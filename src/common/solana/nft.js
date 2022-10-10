c9a51b32736285b9186f1b051881d412d5cb2076
kieudaithien1 <thienkieu@yggsea.io>
2022-10-10 17:57:07 +0700

    init test create nft and test transfer nft for mix-mox-max

--- src/common/solana/nft.js
+++ src/common/solana/nft.js
@@ -1,86 +1,115 @@
-const { clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey } = require('@solana/web3.js');
-const { createMint, getOrCreateAssociatedTokenAccount, mintTo, transfer } = require('@solana/spl-token');
-const { Metaplex } = require("@metaplex-foundation/js");
-const connection = new Connection(clusterApiUrl("mainnet-beta"));
+const {
+  clusterApiUrl,
+  Connection,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+} = require("@solana/web3.js");
+const {
+  createMint,
+  getOrCreateAssociatedTokenAccount,
+  mintTo,
+  transfer,
+} = require("@solana/spl-token");
+const { Metaplex, keypairIdentity } = require("@metaplex-foundation/js");
+const connection = new Connection(clusterApiUrl("devnet"));
 const axios = require("axios");
-const { AppendDataToFile, getDataFromFileTxt } = require("../utils");
-const fs = require('fs')
-const path = require('path')
-const RESULT_PATH = path.join(__dirname, './result.txt')
+const {
+  AppendDataToFile,
+  getDataFromFileTxt,
+  runMutiThreadForLoop,
+} = require("../utils");
+const fs = require("fs");
+const path = require("path");
+const RESULT_PATH = path.join(__dirname, "./result.txt");
 
-
-async function getAllNftByOwner(owner){
-    const metaplex = new Metaplex(connection)
-    const nft = await metaplex.nfts().findAllByOwner({owner}).run();
-    return nft
-    
+async function getAllNftByOwner(owner) {
+  const metaplex = new Metaplex(connection);
+  const nft = await metaplex.nfts().findAllByOwner({ owner }).run();
+  return nft;
 }
-async function getMetadataByOwner(owner, symbol){
-    const nfts = await getAllNftByOwner(owner)
-    const heighStartEnd = parseInt(nfts.length / 10) + 1
-    var promises = [];
-    var start = 0, end = heighStartEnd
-    for(let i = 0 ; i < 10 ; i++){
-        promises.push(getMetaDataFromUri(nfts, start, end, symbol))
-        start = end;
-        end = (heighStartEnd + end > nfts.length) ? (nfts.length) : (heighStartEnd + end)
-    }
-    await Promise.all(promises)
-    let result = await getDataFromFileTxt(RESULT_PATH)
-    fs.unlinkSync(RESULT_PATH)
-    return result
+async function getMetadataByOwner(owner, symbol) {
+  const nfts = await getAllNftByOwner(owner);
+  const heighStartEnd = parseInt(nfts.length / 10) + 1;
+  var promises = [];
+  var start = 0,
+    end = heighStartEnd;
+  for (let i = 0; i < 10; i++) {
+    promises.push(getMetaDataFromUri(nfts, start, end, symbol));
+    start = end;
+    end = heighStartEnd + end > nfts.length ? nfts.length : heighStartEnd + end;
+  }
+  await Promise.all(promises);
+  let result = await getDataFromFileTxt(RESULT_PATH);
+  fs.unlinkSync(RESULT_PATH);
+  return result;
 }
-async function getMetaDataFromUri(nfts, fromIdx, toIdx, symbol){
-    const result = []
-    for (let i = fromIdx ; i< toIdx ; i++) {
-        if (nfts[i].symbol != symbol) continue;
-        const { data } = await axios.get(nfts[i].uri)
-        metadata = data
-        AppendDataToFile(RESULT_PATH, JSON.stringify(metadata) + "\n")
-       result.push(metadata)
-    }
-    return result
+async function getMetaDataFromUri(nfts, fromIdx, toIdx, symbol) {
+  const result = [];
+  for (let i = fromIdx; i < toIdx; i++) {
+    if (nfts[i].symbol != symbol) continue;
+    const { data } = await axios.get(nfts[i].uri);
+    metadata = data;
+    AppendDataToFile(RESULT_PATH, JSON.stringify(metadata) + "\n");
+    result.push(metadata);
+  }
+  return result;
 }
-
 
 async function transferNFT(connection, fromWallet, mint, toWallet) {
-    const toWalletPublicKey = new PublicKey(toWallet)
-    // Get the token account of the fromWallet address, and if it does not exist, create it
-    const fromTokenAccount = await getOrCreateAssociatedTokenAccount(
-        connection,
-        fromWallet,
-        mint,
-        fromWallet.publicKey
-    );
-    console.log('fromTokenAccount', fromTokenAccount.address.toString())
+  const toWalletPublicKey = new PublicKey(toWallet);
+  // Get the token account of the fromWallet address, and if it does not exist, create it
+  const fromTokenAccount = await getOrCreateAssociatedTokenAccount(
+    connection,
+    fromWallet,
+    mint,
+    fromWallet.publicKey
+  );
+  console.log("fromTokenAccount", fromTokenAccount.address.toString());
 
-    // Get the token account of the toWallet address, and if it does not exist, create it
-    const toTokenAccount = await getOrCreateAssociatedTokenAccount(
-        connection,
-        fromWallet,
-        mint,
-        toWalletPublicKey
-    );
-    console.log('toTokenAccount', toTokenAccount.address.toString())
+  // Get the token account of the toWallet address, and if it does not exist, create it
+  const toTokenAccount = await getOrCreateAssociatedTokenAccount(
+    connection,
+    fromWallet,
+    mint,
+    toWalletPublicKey
+  );
+  console.log("toTokenAccount", toTokenAccount.address.toString());
 
-    // Transfer the new token to the "toTokenAccount" we just created
-    signature = await transfer(
-        connection,
-        fromWallet,
-        fromTokenAccount.address,
-        toTokenAccount.address,
-        fromWallet,
-        1,
-        [],
-        { skipPreflight: true, commitment: 'processed' }
-    );
-    return signature;
+  // Transfer the new token to the "toTokenAccount" we just created
+  signature = await transfer(
+    connection,
+    fromWallet,
+    fromTokenAccount.address,
+    toTokenAccount.address,
+    fromWallet,
+    1,
+    [],
+    { skipPreflight: true, commitment: "processed" }
+  );
+  
+  return signature;
 }
-
-module.exports = { transferNFT };
+async function createListNftTest(connection, owner, uriBase, symbol, total) {
+  const metaplex = new Metaplex(connection);
+  metaplex.use(keypairIdentity(owner));
+  for (let i = 0; i < 100; i++) {
+    const mintNFTResponse = await metaplex
+      .nfts()
+      .create({
+        uri: uriBase,
+        name: symbol + i,
+        symbol: symbol,
+        maxSupply: 1,
+      })
+      .run();
+    console.log(mintNFTResponse);
+  }
+}
 
 module.exports = {
   getAllNftByOwner,
   getMetadataByOwner,
-  transferNFT
-}
+  transferNFT,
+  createListNftTest,
+};
--- src/task-solana/mix-mox-max/transferAllNft.js
+++ src/task-solana/mix-mox-max/transferAllNft.js
@@ -1,23 +1,33 @@
-const { Connection, clusterApiUrl } = require("@solana/web3.js");
-require('dotenv').config({ path: path.join(__dirname, '../../.env') });
+const { Connection, clusterApiUrl, Keypair } = require("@solana/web3.js");
+const bs58 = require('bs58')
+const path = require('path')
+require('dotenv').config({ path: path.join(__dirname, '../../../.env') });
 const { default: axios } = require("axios");
-const { transferNFT } = require("../../common/solana/nft");
+const { transferNFT, getAllNftByOwner } = require("../../common/solana/nft");
 const {runMutiThreadForLoop } = require("../../common/utils");
-const connection = new Connection(clusterApiUrl("mainnet-beta"));
-const ownerKeyPair = process.env.YGGSEA
+const ownerKeyPair = Keypair.fromSecretKey(bs58.decode(process.env.ACCOUNT1_PRIVATE_KEY));
+
+//const connection = new Connection(clusterApiUrl("devnet"));
+
+//TEST
+const connection = new Connection(clusterApiUrl("devnet"));
+
 
 main().catch(err => console.log(err))
 let nfts = []
 async function main(){
-    let { data } = await axios.get(`https://api.solscan.io/account/v2/tokenaccounts/total?address=${ownerKeyPair.publicKey}&cluster=`)
-    const totalNft = data.data
-    //get all nft
-    await runMutiThreadForLoop(totalNft, 0, getNfts)
+    // let { data } = await axios.get(`https://api.solscan.io/account/v2/tokenaccounts/total?address=${ownerKeyPair.publicKey}&cluster=`)
+    // const totalNft = data.data
+    // //get all nft
+    // await runMutiThreadForLoop(totalNft, 0, getNfts)
     // transfer all nft
-    await runMutiThreadForLoop(nfts.length, 0, transferAllNft)
-    
-}
+    //await runMutiThreadForLoop(nfts.length, 0, transferAllNft)
 
+    //test
+    nfts = await getAllNftByOwner(ownerKeyPair.publicKey)
+    await runMutiThreadForLoop(nfts.length, 0, transferAllNft)
+
+}
 
 async function getNfts(start, end) {
     var dataHandle = [];
@@ -48,8 +58,16 @@ async function getNfts(start, end) {
 
 async function transferAllNft(start, end){
     for(let i = start; i < end; i++){
-        if(nfts[i].tokenSymbol != 'MXM')
+        // if(nfts[i].tokenSymbol != 'MXM')
+        //     continue
+        //await transferNFT(connection, fromWallet, nfts[i].tokenAddress, 'ACPpwoSRF4B4Ftsjj9TAGxBmJwkHZNb13jnYdEiRshmA')
+
+
+        //TEST
+        if(!nfts[i].name.includes('MXM')){
             continue
-        await transferNFT(connection, fromWallet, nfts[i].tokenAddress, 'ACPpwoSRF4B4Ftsjj9TAGxBmJwkHZNb13jnYdEiRshmA')
+        }
+        await transferNFT(connection, ownerKeyPair, nfts[i].mintAddress, 'HbUfzDvVH3r5Y28i7U3XRCFfsud1ZwizaGKP5cAVgn9')
+
     }
 }
